43a73a0d9b1e0be4518d04e8766c0447839b53f8
awwal <leyeawwal@gmail.com>
2025-05-19 15:24:57 +0100

    feature/added the summary and credit

--- model/setup.js
+++ model/setup.js
@@ -3,12 +3,12 @@ const pool = require("./db");
 const createUsersTable = async () => {
 	try {
 		await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			email VARCHAR(255) UNIQUE NOT NULL,
+			password TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)
     `);
 		console.log("Users table created");
 	} catch (err) {
@@ -30,9 +30,45 @@ const alterUsersTable = async () => {
 	}
 };
 
+const UserSummaryText = async () => {
+	try {
+		await pool.query(`
+		CREATE TABLE summary_texts (
+			id SERIAL PRIMARY KEY,
+			user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+			title TEXT NOT NULL,
+			original_text TEXT NOT NULL,
+			summary TEXT,
+			created_at TIMESTAMP DEFAULT NOW()
+		);
+		`);
+			console.log("User summary text table created successfully")
+	}catch (err) {
+		console.error("Error in creating user summary text table")
+	}
+}
+
+const UserCreditTable = async () => {
+	try {
+		await pool.query(`
+		CREATE TABLE user_credits (
+			id SERIAL PRIMARY KEY,
+			user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+			balance INTEGER NOT NULL DEFAULT 100,
+			last_updated TIMESTAMP DEFAULT NOW()
+		);
+		`);
+			console.log("User credit table created successfully")
+	}catch (err) {
+		console.error("Error in creating user credit table")
+	}
+}
+
 const main = async () => {
 	await createUsersTable();
 	await alterUsersTable();
+	await UserSummaryText();
+	await UserCreditTable()
 	await pool.end();
 };
 
